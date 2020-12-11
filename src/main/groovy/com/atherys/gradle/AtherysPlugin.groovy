5f29186b46615a0d72ae2fe44571f26f4c498211
HaedHutner <mvvasilev@outlook.com>
2020-12-11 20:13:59 +0200

    1.6.0: AtherysCore 2.0.0 and Sponge 7.3.0

--- src/main/groovy/com/atherys/gradle/AtherysPlugin.groovy
+++ src/main/groovy/com/atherys/gradle/AtherysPlugin.groovy
@@ -35,10 +35,10 @@ class AtherysPlugin implements Plugin<Project> {
         def shadow = project.configurations["shadow"]
         def deps = project.dependencies
 
-        shadow.dependencies.add(deps.create("org.spongepowered:spongeapi:7.2.0"))
+        shadow.dependencies.add(deps.create("org.spongepowered:spongeapi:7.3.0"))
 
         if (project.name != "AtherysCore") {
-            shadow.dependencies.add(deps.create("com.github.Atherys-Horizons:AtherysCore:1.19.0"))
+            shadow.dependencies.add(deps.create("com.github.Atherys-Horizons:AtherysCore:2.0.0"))
         }
 
         project.tasks.create("atherysdoc", Javadoc.class, { task ->
@@ -66,7 +66,7 @@ class AtherysPlugin implements Plugin<Project> {
                 "https://google.github.io/guice/api-docs/4.1/javadoc/",
                 "https://google.github.io/guava/releases/21.0/api/docs/",
                 "https://docs.oracle.com/javase/8/docs/api/",
-                "https://jd.spongepowered.org/7.1.0/",
+                "https://jd.spongepowered.org/7.3.0/",
                 "https://docs.atherys.com/javadocs/AtherysCore/"
         ])
 
